81611ed6b2f9dbe63b2aa72313386975c2aaf0cd
saikumar <sai.nunnagoppula345@gmail.com>
2021-08-31 16:49:05 +0530

    add new changes

--- js/registration.js
+++ js/registration.js
@@ -13,12 +13,23 @@ function validateMobile() {
     return;
   }
 }
-
 function updateUser() {
   const updatedFname = document.getElementById("fname").value;
   const updatedLname = document.getElementById("lname").value;
+  const updatedgender = document.getElementById("radio1").checked
+    ? "Male"
+    : "Female";
   const updatedMobile = document.getElementById("mobile").value;
   const updatedEmail = document.getElementById("email").value;
+  var e = document.getElementById("speification");
+  var updatedspecification = e.options[e.selectedIndex].text;
+  var checkboxes = document.getElementsByName("language");
+  var updatedselectedlanguages = [];
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      updatedselectedlanguages.push(checkboxes[i].value);
+    }
+  }
   let table = document.getElementById("userDetails");
   let index = storeDetais.findIndex(
     (item) => item.mobile == updatedMobileNumber
@@ -26,11 +37,22 @@ function updateUser() {
   storeDetais.splice(index, 1, {
     fName: updatedFname,
     lName: updatedLname,
+    gender: updatedgender,
     mobile: updatedMobile,
     email: updatedEmail,
+    specification: updatedspecification,
+    selectedlanguages: updatedselectedlanguages,
   });
   deleteUser(updatedMobileNumber, "updating");
-  updateTableDOM(updatedFname, updatedLname, updatedMobile, updatedEmail);
+  updateTableDOM(
+    updatedFname,
+    updatedLname,
+    updatedgender,
+    updatedMobile,
+    updatedEmail,
+    updatedspecification,
+    updatedselectedlanguages
+  );
   clearFormData();
   document.getElementById("saveButtons").classList.toggle("hidden");
   document.getElementById("updateButton").classList.toggle("hidden");
@@ -57,9 +79,24 @@ function editUser(mobileNumber) {
   const userData = storeDetais.find((item) => item.mobile == mobileNumber);
   document.getElementById("fname").value = userData.fName;
   document.getElementById("lname").value = userData.lName;
+  if (userData.gender == "male") {
+    document.getElementById("radio1").checked = true;
+    document.getElementById("radio2").checked = false;
+  } else {
+    document.getElementById("radio1").checked = false;
+    document.getElementById("radio2").checked = true;
+  }
   document.getElementById("mobile").value = userData.mobile;
   document.getElementById("email").value = userData.email;
-
+  document.getElementById("speification").value = userData.specification;
+  console.log(userData.selectedLanguages); // ['React']
+  document.getElementsByName("language").forEach((checkbox) => {
+    if (userData.selectedLanguages.includes(checkbox.value)) {
+      checkbox.checked = true;
+    } else {
+      checkbox.checked = false;
+    }
+  });
   document.getElementById("mobile").setAttribute("disabled", true);
   document.getElementById("saveButtons").classList.toggle("hidden");
   document.getElementById("updateButton").classList.toggle("hidden");
@@ -93,7 +130,20 @@ function registerUser() {
   const lname = document.getElementById("lname").value;
   const gender = document.getElementById("radio1").checked ? "Male" : "Female";
   const mobile = document.getElementById("mobile").value;
+  console.log(typeof mobile);
+  if (typeof mobile === "string") {
+    alert("Please Fill the Mobile Number as a intiger");
+    return;
+  }
   const email = document.getElementById("email").value;
+  var e = document.getElementById("speification");
+  var specification = e.options[e.selectedIndex].text; //index=0,1:value=mecha,elect:text=mech.elect
+  const selectedLanguages = [];
+  document.getElementsByName("language").forEach((checkbox) => {
+    if (checkbox.checked) {
+      selectedLanguages.push(checkbox.value);
+    }
+  });
 
   const userData = {
     fName: fname,
@@ -101,6 +151,8 @@ function registerUser() {
     gender: gender,
     mobile: mobile,
     email: email,
+    specification: specification,
+    selectedLanguages: selectedLanguages,
   };
   storeDetais.push(userData);
   // Reset Data after register user.
@@ -109,7 +161,15 @@ function registerUser() {
     alert("Please Fill The Details");
     return;
   }
-  updateTableDOM(fname, lname, gender, mobile, email);
+  updateTableDOM(
+    fname,
+    lname,
+    gender,
+    mobile,
+    email,
+    specification,
+    selectedLanguages
+  );
 
   document
     .getElementsByClassName("saved-message")[0]
@@ -122,7 +182,15 @@ function registerUser() {
   }
 }
 
-function updateTableDOM(firstName, lastName, gender, mobile, email) {
+function updateTableDOM(
+  firstName,
+  lastName,
+  gender,
+  mobile,
+  email,
+  specification,
+  selectedLanguages
+) {
   const table = document.getElementById("userDetails");
   table.innerHTML =
     table.innerHTML +
@@ -141,6 +209,12 @@ function updateTableDOM(firstName, lastName, gender, mobile, email) {
     "<td>" +
     email +
     "</td>" +
+    "<td>" +
+    specification +
+    "</td>" +
+    "<td>" +
+    selectedLanguages.join(", ") +
+    "</td>" +
     "<td><img onclick='editUser(" +
     mobile +
     ")' src='../images/edit.png' class='action-image' width='25px' height='25px'/><img onclick='deleteUser(" +
@@ -151,6 +225,12 @@ function updateTableDOM(firstName, lastName, gender, mobile, email) {
 function clearFormData() {
   document.getElementById("fname").value = "";
   document.getElementById("lname").value = "";
+  document.getElementById("radio1").checked = true;
+  document.getElementById("radio2").checked = false;
   document.getElementById("mobile").value = "";
   document.getElementById("email").value = "";
+  document.getElementById("speification").value = "";
+  document.getElementsByName("language").forEach((item) => {
+    item.checked = false;
+  });
 }
